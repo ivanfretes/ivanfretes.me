c06c8e69d26107c11ddb028232e96f1972845faf
Iván Fretes <vanfretes@gmail.com>
2022-07-30 17:53:17 -0400

    feat: add vercel title

--- components/base/Footer.tsx
+++ components/base/Footer.tsx
@@ -3,7 +3,9 @@ export const Footer = () => {
       <footer className="footer mt-5">
          <div className="content has-text-centered">
             <p>
-               <strong>{'Iván Fretes'}</strong><br></br> Hecho con ♥ en React + Next + Bulma
+               <strong>{'Iván Fretes'}</strong><br></br> 
+               Hecho con ♥ en React + Next + Bulma. <br></br>
+               Hospedado en Vercel
             </p>
          </div>
       </footer>
